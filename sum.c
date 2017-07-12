5dbc5213a23553937c58292170514e2aa192433e
gmlrnjs1994 <gmlrnjs1994@naver.com>
2017-07-12 16:34:21 +0900

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,9 +1,13 @@
 #include "sum.h"
 int sum(int n) {
+	/*
 	int result = 0;
+	
 	int i = 0;
 	for(i = 1; i <= n; i ++){
 		result = result + i;
-	}
-	return result;
+	}*/
+	
+	
+	return n*(n+1)/2;
 }
